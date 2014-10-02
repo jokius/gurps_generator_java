d4f0af0afe71e9d519f2110f319adf367a7cf91f
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2014-10-02 13:17:59 +0400

    Change default screen size

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("GURPSGenerator");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 641, 224));
         primaryStage.show();
     }
 
