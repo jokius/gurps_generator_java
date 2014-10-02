fc922ad0bbabf09f5bff13a4a78dad498afa9ffd
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2014-10-02 13:16:26 +0400

    Rename main_menu view, part interface

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main_menu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("GURPSGenerator");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
