af0de55e9b9dde92215b555b719e8bf4e6b4c37b
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2014-12-04 14:38:52 +0300

    Add db config

---
+++ src/ru/gurps/generator/config/Db.java
@@ -0,0 +1,19 @@
+package ru.gurps.generator.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Db {
+    public static Connection connect = null;
+
+    public Db() {
+        try {
+            Class.forName("org.h2.Driver").newInstance();
+            String parent = "\\w*.jar";
+            String s = ru.gurps.generator.Main.class.getProtectionDomain().getCodeSource().getLocation().getPath().replaceAll(parent, "");
+            connect = DriverManager.getConnection("jdbc:h2:"+ s + "db/gurps", "sa", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
