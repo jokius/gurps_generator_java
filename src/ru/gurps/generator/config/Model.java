dd8397e7fe2c301536c9edb01011ba5f901a3ff7
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-03-13 17:11:31 +0300

    Change functions create, all; add function setModel

--- src/ru/gurps/generator/config/Model.java
+++ src/ru/gurps/generator/config/Model.java
@@ -1,5 +1,8 @@
 package ru.gurps.generator.config;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -31,23 +34,7 @@ public class Model extends Db {
             connect.createStatement().executeUpdate("INSERT INTO " + table + " SET " + params);
             result = connect.createStatement().executeQuery("SELECT * FROM " + table + " WHERE id=" + id);
             result.next();
-
-            for (Field field : this.getClass().getDeclaredFields()) {
-                try {
-                    if (String.class.isAssignableFrom(field.getType()))
-                        field.set(this, result.getString(field.getName()));
-                    else if (Integer.class.isAssignableFrom(field.getType()))
-                        field.set(this, result.getInt(field.getName()));
-                    else if (Double.class.isAssignableFrom(field.getType()))
-                        field.set(this, result.getDouble(field.getName()));
-                    else if (Boolean.class.isAssignableFrom(field.getType()))
-                        field.set(this, result.getBoolean(field.getName()));
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            return this;
+            return setModel(result);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -113,10 +100,30 @@ public class Model extends Db {
         return false;
     }
 
-    public ResultSet all() {
+    public boolean delete() {
+        try {
+            int id = (int) this.getClass().getDeclaredField("id").get(this);
+            createConnection();
+            connect.createStatement().executeUpdate("DELETE FROM " + table + " WHERE id=" + id);
+            return true;
+        } catch (SQLException | IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public ObservableList all() {
         try {
             createConnection();
-            return connect.createStatement().executeQuery("SELECT * FROM " + table);
+            ResultSet results = connect.createStatement().executeQuery("SELECT * FROM " + table);
+
+            ObservableList list = FXCollections.observableArrayList();
+            while (results.next()) {
+                list.add(setModel(results));
+            }
+
+            return list;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -188,5 +195,23 @@ public class Model extends Db {
             e.printStackTrace();
         }
         return true;
+    }
+
+    private Model setModel(ResultSet result) throws SQLException {
+        for (Field field : this.getClass().getDeclaredFields()) {
+            try {
+                if (String.class.isAssignableFrom(field.getType()))
+                    field.set(this, result.getString(field.getName()));
+                else if (Integer.class.isAssignableFrom(field.getType()))
+                    field.set(this, result.getInt(field.getName()));
+                else if (Double.class.isAssignableFrom(field.getType()))
+                    field.set(this, result.getDouble(field.getName()));
+                else if (Boolean.class.isAssignableFrom(field.getType()))
+                    field.set(this, result.getBoolean(field.getName()));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return this;
     }
 }
