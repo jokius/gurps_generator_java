dc62fc59c970268fb926bd69b0b1ff8079589d3f
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-03-17 18:17:16 +0300

    Change User add fields

--- src/ru/gurps/generator/models/User.java
+++ src/ru/gurps/generator/models/User.java
@@ -4,6 +4,7 @@ import ru.gurps.generator.config.Model;
 
 public class User extends Model {
     public Integer id;
+    public String player;
     public String name;
     public String currentPoints;
     public String maxPoints;
@@ -18,6 +19,17 @@ public class User extends Model {
     public Double bs = 5.0;
     public Integer move = 5;
     public Integer sm = 0;
+    public Integer growth = 0;
+    public Integer weight = 0;
+    public Integer age = 0;
+    public Integer tl = 0;
+    public Integer tlCost = 0;
+    public Integer head = 0;
+    public Integer torse = 0;
+    public Integer arm = 0;
+    public Integer leg = 0;
+    public Integer hand = 0;
+    public Integer foot = 0;
     public Boolean noFineManipulators = false;
     
     public User(){
