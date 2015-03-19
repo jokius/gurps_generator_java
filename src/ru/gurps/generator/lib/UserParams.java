3642b098da19bd6df80af67d9831f9aee8beda0f
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-03-19 13:49:22 +0300

    Change UserParams: add setParry(), setBlock()

--- src/ru/gurps/generator/lib/UserParams.java
+++ src/ru/gurps/generator/lib/UserParams.java
@@ -1,7 +1,9 @@
 package ru.gurps.generator.lib;
 
+import javafx.collections.ObservableList;
 import javafx.scene.control.Label;
 import ru.gurps.generator.controller.UsersController;
+import ru.gurps.generator.models.Skill;
 import ru.gurps.generator.models.User;
 
 public class UserParams {
@@ -20,6 +22,8 @@ public class UserParams {
     private Label doge;
     private Label thrust;
     private Label swing;
+    private Label block;
+    private Label parry;
 
     public UserParams(Label stCost, Label dxCost, Label iqCost, Label htCost, Label hpCost, Label willCost, Label perCost, Label fpCost, Label bsCost, Label moveCost, Label bg, Label doge, Label thrust, Label swing) {
         this.stCost = stCost;
@@ -38,7 +42,7 @@ public class UserParams {
         this.swing = swing;
     }
 
-    public UserParams(Label stCost, Label dxCost, Label iqCost, Label htCost, Label hpCost, Label willCost, Label perCost, Label fpCost, Label bg, Label doge, Label thrust, Label swing) {
+    public UserParams(Label stCost, Label dxCost, Label iqCost, Label htCost, Label hpCost, Label willCost, Label perCost, Label fpCost, Label bsCost, Label moveCost, Label bg, Label doge, Label thrust, Label swing, Label block, Label parry) {
         this.stCost = stCost;
         this.dxCost = dxCost;
         this.iqCost = iqCost;
@@ -47,10 +51,14 @@ public class UserParams {
         this.willCost = willCost;
         this.perCost = perCost;
         this.fpCost = fpCost;
+        this.bsCost = bsCost;
+        this.moveCost = moveCost;
         this.bg = bg;
         this.doge = doge;
         this.thrust = thrust;
         this.swing = swing;
+        this.block = block;
+        this.parry = parry;
     }
 
     public void setSt() {
@@ -138,5 +146,29 @@ public class UserParams {
     public void setDmg() {
         thrust.setText(Dmg.thrust(user.st));
         swing.setText(Dmg.swing(user.st));
+    }
+
+    public void setParry(ObservableList<Skill> skills) {
+        int resultSkill = user.dx - 4;
+        for(Skill skill : skills)
+            if(skill.parry && skill.level > resultSkill) resultSkill = skill.level + skill.parryBonus;
+        parry.setText(Integer.toString(3 + resultSkill / 2));
+    }
+
+
+    public void setBlock(ObservableList<Skill> skills) {
+        int shield = user.dx - 4;
+        int cloak = 0;
+        for(Skill skill : skills){
+            switch(skill.nameEn){
+                case "Shield": shield = skill.level;
+                case "Cloak": cloak = skill.level;
+                case "Net": if(cloak == 0) cloak = skill.level - 4;
+            }
+        }
+
+        if(cloak == 0) cloak = user.dx - 5;
+        if(shield >= cloak) block.setText(Integer.toString(3 + shield / 2));
+        else block.setText(Integer.toString(3 + cloak / 2));
     }
 }
