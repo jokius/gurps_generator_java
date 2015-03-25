2f50dc049236604c0aa249f58aabbe341dfa0ede
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-03-25 19:32:09 +0300

    Add JsonUserFeature pojo

---
+++ src/ru/gurps/generator/lib/export/pojo/JsonUserFeature.java
@@ -0,0 +1,20 @@
+package ru.gurps.generator.lib.export.pojo;
+
+import javafx.collections.ObservableList;
+import ru.gurps.generator.models.FeatureAddon;
+import ru.gurps.generator.models.UserFeature;
+
+public class JsonUserFeature {
+    public UserFeature userFeature;
+    public ObservableList<FeatureAddon> featureAddons;
+
+    public JsonUserFeature() {
+    }
+}
+
+
+
+
+
+
+
