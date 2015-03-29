eee234c1fe17bf443a974e93dd2495069fe71587
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-03-29 22:03:17 +0300

    Change Spell: add params

--- src/ru/gurps/generator/models/Spell.java
+++ src/ru/gurps/generator/models/Spell.java
@@ -4,10 +4,22 @@ import ru.gurps.generator.config.Model;
 
 public class Spell extends Model {
     public Integer id;
+    public Integer school;
     public String name;
+    public String nameEn;
+    public Integer spellType;
+    public String description;
     public Integer complexity;
     public Integer cost;
-    @Ignore public Integer level;
+    public Integer maxCost;
+    public String needTime;
+    public String duration;
+    public String maintainingCost;
+    public String thing;
+    public String createCost;
+    public String demands;
+    @Ignore public Integer level = 1;
+    @Ignore public Integer finalCost = 0;
     @Ignore public Boolean add = false;
 
     public Spell() {
@@ -15,6 +27,73 @@ public class Spell extends Model {
 
     public String getName() {
         return name;
+    }
+
+    public String getNameEn() {
+        return nameEn;
+    }
+
+    public String getNeedTime() {
+        return needTime;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public String getMaintainingCost() {
+        return maintainingCost;
+    }
+
+    public String getThing() {
+        return thing;
+    }
+
+    public String getCreateCost() {
+        return createCost;
+    }
+
+    public String getDemands() {
+        return demands;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getFinalCost() {
+        return finalCost;
+    }
+
+    public String getCost() {
+        if(cost == maxCost) return Integer.toString(cost);
+        else return cost + " - " + maxCost;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public String getSchool() {
+        switch(school){
+            case 0: return "Воздух";
+        }
+        return null;
+    }
+
+    public String getSpellType() {
+        switch(spellType){
+            case 0: return "Обычное";
+            case 1: return "Областное";
+            case 2: return "Касательное";
+            case 3: return "Метательное";
+            case 4: return "Блокирующие";
+            case 5: return "Сопротивляемое";
+            case 6: return "Информационное";
+            case 7: return "Чары";
+            case 8: return "Особое";
+        }
+        return null;
     }
 
     public String getComplexity() {
@@ -27,6 +106,16 @@ public class Spell extends Model {
         return null;
     }
 
+    public String getComplexityFull() {
+        switch(complexity){
+            case 0: return "Ллегко";
+            case 1: return "Средне";
+            case 2: return "Трудно";
+            case 3: return "Очень Трудно";
+        }
+        return null;
+    }
+
     public void setСomplexity(String complexity) {
         switch(complexity){
             case "Л": this.complexity = 0;
@@ -34,13 +123,5 @@ public class Spell extends Model {
             case "Т": this.complexity = 2;
             case "ОТ": this.complexity = 3;
         }
-    }
-
-    public Integer getCost() {
-        return cost;
-    }
-
-    public Integer getLevel() {
-        return level;
     }
 }
