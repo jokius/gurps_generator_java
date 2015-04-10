6b9b7f19778254bb1ef3e509c5e1126ac10d7009
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-04-10 19:44:48 +0300

    Add Quirk model

---
+++ src/ru/gurps/generator/models/Quirk.java
@@ -0,0 +1,29 @@
+package ru.gurps.generator.models;
+
+import ru.gurps.generator.config.Model;
+
+public class Quirk extends Model {
+    public Integer id;
+    public String name;
+    @Ignore public Integer cost = 0;
+    @Ignore public Boolean add = false;
+
+    public Quirk() {
+    }
+
+    public Quirk(String language) {
+        this.name = language;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCost() {
+        return Integer.toString(cost);
+    }
+
+    public Boolean getAdd() {
+        return add;
+    }
+}
