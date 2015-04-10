87bbde346e94a86c0b6d3275fa249f639088824f
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-04-10 16:45:07 +0300

    Remove unnecessary parameters and functions

--- src/ru/gurps/generator/controller/AbstractController.java
+++ src/ru/gurps/generator/controller/AbstractController.java
@@ -1,35 +1,9 @@
 package ru.gurps.generator.controller;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.stage.Stage;
-import ru.gurps.generator.Main;
-import ru.gurps.generator.lib.*;
-import ru.gurps.generator.models.*;
-import java.io.IOException;
+import javafx.scene.control.Label;
+import ru.gurps.generator.models.User;
 
-public class AbstractController extends ViewsAbstract {
+public class AbstractController {
     public static User user;
     public static Label globalCost;
-
-    protected void buttonEvents() {
-        userSheet.setOnAction(event -> {
-            Stage childrenStage = new Stage();
-            userSheet.setDisable(true);
-            childrenStage.setOnCloseRequest(we -> userSheet.setDisable(false));
-            FXMLLoader loader = new FXMLLoader(Main.class.getResource("resources/views/userSheet.fxml"));
-            Parent childrenRoot;
-            try {
-                childrenRoot = loader.load();
-                childrenStage.setScene(new Scene(childrenRoot, 700, 795));
-                childrenStage.setTitle("GURPS Лист персонажа");
-                childrenStage.show();
-                childrenStage.setResizable(false);
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
-        });
-    }
 }
--- src/ru/gurps/generator/controller/MainWindowController.java
+++ src/ru/gurps/generator/controller/MainWindowController.java
@@ -1,21 +1,47 @@
 package ru.gurps.generator.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import ru.gurps.generator.lib.TabConfigure;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import ru.gurps.generator.Main;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
 
 public class MainWindowController extends AbstractController {
+    // global part
+    public TextField maxPoints;
+    public Label currentPoints;
+    public Button userSheet;
+
+    // menus
+    public Menu viewMenu;
+
+    // tabs
+    public TabPane mainTabPanel;
+
+    public Tab paramsTab;
+    public Tab advantagesTab;
+    public Tab disadvantagesTab;
+    public Tab modesTab;
+    public Tab skillsTab;
+    public Tab techniquesTab;
+    public Tab spellTab;
+    public Tab languagesTab;
+    public Tab culturasTab;
+    public Tab equipmentTab;
+
     @FXML
     public void initialize() {
         globalCost = currentPoints;
-
-        new TabConfigure(viewMenu, mainTabPanel, paramsTab, advantagesTab, disadvantagesTab, modesTab, skillsTab,
-                techniquesTab, spellTab, languagesTab, culturasTab, equipmentTab);
-
-        buttonEvents();
+        tabsConfigure();
         maxPoints.setText(user.maxPoints);
-
-        
-        currentPoints.setText(user.currentPoints);
+        globalCost.setText(user.currentPoints);
 
         maxPoints.textProperty().addListener((observable, oldValue, newValue) -> {
             if(newValue.equals("")) return;
@@ -29,5 +55,52 @@ public class MainWindowController extends AbstractController {
             maxPoints.setText(newValue);
             user.save();
         });
+
+        userSheet.setOnAction(event -> {
+            Stage childrenStage = new Stage();
+            userSheet.setDisable(true);
+            childrenStage.setOnCloseRequest(we -> userSheet.setDisable(false));
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("resources/views/userSheet.fxml"));
+            Parent childrenRoot;
+            try {
+                childrenRoot = loader.load();
+                childrenStage.setScene(new Scene(childrenRoot, 700, 795));
+                childrenStage.setTitle("GURPS Лист персонажа");
+                childrenStage.show();
+                childrenStage.setResizable(false);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void tabsConfigure(){
+        ObservableList<CheckMenuItem> checkMenuItems = FXCollections.observableArrayList();
+        for(Field field : this.getClass().getDeclaredFields()){
+            if(Tab.class.isAssignableFrom(field.getType())){
+                try {
+                    Tab tab = (Tab) field.get(this);
+                    CheckMenuItem checkMenuItem = new CheckMenuItem(tab.getText());
+                    checkMenuItem.setSelected(false);
+                    for(Tab viewTab : mainTabPanel.getTabs()){
+                        if(viewTab == tab){
+                            checkMenuItem.setSelected(true);
+                            break;
+                        }
+                    }
+
+                    checkMenuItem.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                        if(newValue) mainTabPanel.getTabs().add(tab);
+                        else mainTabPanel.getTabs().remove(tab);
+                    });
+
+                    tab.setOnClosed(event -> checkMenuItem.setSelected(false));
+                    checkMenuItems.add(checkMenuItem);
+                } catch(IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        viewMenu.getItems().addAll(checkMenuItems);
     }
 }
