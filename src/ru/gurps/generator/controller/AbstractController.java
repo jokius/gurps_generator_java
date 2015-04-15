8dcaa9cff96f24c790b63376f27d9729c96afc6f
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-04-15 13:13:26 +0300

    Change AbstractController: add  function setCurrentPoints()

--- src/ru/gurps/generator/controller/AbstractController.java
+++ src/ru/gurps/generator/controller/AbstractController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import ru.gurps.generator.Main;
 import ru.gurps.generator.models.User;
@@ -17,11 +18,11 @@ public class AbstractController extends Main {
     protected void createMainStage(){
         Stage childrenStage = new Stage();
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("resources/views/main.fxml"));
-        childrenStage.setMinWidth(640);
-        childrenStage.setMinHeight(292);
+        childrenStage.setMinWidth(670);
+        childrenStage.setMinHeight(560);
         try {
             Parent childrenRoot = loader.load();
-            childrenStage.setScene(new Scene(childrenRoot, 650, 500));
+            childrenStage.setScene(new Scene(childrenRoot, 670, 540));
             childrenStage.setTitle("GURPSGenerator");
             childrenStage.show();
         } catch (IOException e) {
@@ -44,5 +45,13 @@ public class AbstractController extends Main {
         } catch(IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void setCurrentPoints(int points){
+        String sPoints = Integer.toString(points);
+        globalCost.setText(sPoints);
+        user.update_single("currentPoints", sPoints);
+        if(Integer.parseInt(user.maxPoints) >= points) globalCost.setTextFill(Color.GREEN);
+        else globalCost.setTextFill(Color.RED);
     }
 }
