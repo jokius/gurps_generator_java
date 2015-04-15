16422a35c4886866815d20c2e2e9c08d2c2118df
Igor Kutyavin <igor.kutyavin@saltpepper.ru>
2015-04-15 13:40:28 +0300

    Change UserParams: remove unnecessary functions

--- src/ru/gurps/generator/lib/UserParams.java
+++ src/ru/gurps/generator/lib/UserParams.java
@@ -1,71 +1,10 @@
 package ru.gurps.generator.lib;
 
 import javafx.collections.ObservableList;
-import javafx.scene.control.Label;
 import ru.gurps.generator.controller.AbstractController;
 import ru.gurps.generator.models.Skill;
 
 public class UserParams extends AbstractController {
-    private Label stCost;
-    private Label dxCost;
-    private Label iqCost;
-    private Label htCost;
-    private Label hpCost;
-    private Label willCost;
-    private Label perCost;
-    private Label fpCost;
-    private Label bsCost;
-    private Label moveCost;
-    private Label bg;
-    private Label doge;
-    private Label thrust;
-    private Label swing;
-    private Label block;
-    private Label parry;
-
-    public UserParams() {
-    }
-
-    public UserParams(Label stCost, Label dxCost, Label iqCost, Label htCost, Label hpCost, Label willCost, Label perCost, Label fpCost, Label bsCost, Label moveCost, Label bg, Label doge, Label thrust, Label swing) {
-        this.stCost = stCost;
-        this.dxCost = dxCost;
-        this.iqCost = iqCost;
-        this.htCost = htCost;
-        this.hpCost = hpCost;
-        this.willCost = willCost;
-        this.perCost = perCost;
-        this.fpCost = fpCost;
-        this.bsCost = bsCost;
-        this.moveCost = moveCost;
-        this.bg = bg;
-        this.doge = doge;
-        this.thrust = thrust;
-        this.swing = swing;
-    }
-
-    public UserParams(Label stCost, Label dxCost, Label iqCost, Label htCost, Label hpCost, Label willCost, Label perCost, Label fpCost, Label bsCost, Label moveCost, Label bg, Label doge, Label thrust, Label swing, Label block, Label parry) {
-        this.stCost = stCost;
-        this.dxCost = dxCost;
-        this.iqCost = iqCost;
-        this.htCost = htCost;
-        this.hpCost = hpCost;
-        this.willCost = willCost;
-        this.perCost = perCost;
-        this.fpCost = fpCost;
-        this.bsCost = bsCost;
-        this.moveCost = moveCost;
-        this.bg = bg;
-        this.doge = doge;
-        this.thrust = thrust;
-        this.swing = swing;
-        this.block = block;
-        this.parry = parry;
-    }
-
-    public void setSt() {
-        stCost.setText(Integer.toString(stCost()));
-    }
-
     public static int stCost(){
         int cost = (user.st - 10) * 10;
         if(cost != 0) {
@@ -75,34 +14,18 @@ public class UserParams extends AbstractController {
         return cost;
     }
 
-    public void setDx() {
-        dxCost.setText(Integer.toString(dxCost()));
-    }
-
     public static int dxCost(){
         int cost = (user.dx - 10) * 20;
         if(user.noFineManipulators && cost != 0) cost = (int) (cost - (cost * 0.4));
         return cost;
     }
 
-    public void setIq() {
-        iqCost.setText(Integer.toString(iqCost()));
-    }
-
     public static int iqCost(){
         return (user.iq - 10) * 20;
     }
 
-    public void setHt() {
-        htCost.setText(Integer.toString(htCost()));
-    }
-
     public static int htCost(){
         return (user.ht - 10) * 10;
-    }
-
-    public void setHp() {
-        hpCost.setText(Integer.toString(hpCost()));
     }
 
     public static int hpCost(){
@@ -112,32 +35,16 @@ public class UserParams extends AbstractController {
         return cost;
     }
 
-    public void setWill() {
-        willCost.setText(Integer.toString(willCost()));
-    }
-
     public static int willCost(){
         return (user.will - user.iq) * 5;
-    }
-
-    public void setPer() {
-        perCost.setText(Integer.toString(perCost()));
     }
 
     public static int perCost(){
         return (user.per - user.iq) * 5;
     }
 
-    public void setFp() {
-        fpCost.setText(Integer.toString(fpCost()));
-    }
-
     public static int fpCost(){
         return (user.fp - user.ht) * 3;
-    }
-
-    public void setBs() {
-        bsCost.setText(Integer.toString(bsCost()));
     }
 
     public static double defaultBs()
@@ -164,48 +71,26 @@ public class UserParams extends AbstractController {
         return cost;
     }
 
-    public void setMove() {
-        moveCost.setText(Integer.toString(moveCost()));
-    }
-
     public static int moveCost(){
         return (int) (user.move - user.bs);
-    }
-
-    public void setBg() {
-        bg.setText(Integer.toString(bg()));
     }
 
     public static int bg(){
         return (user.st * user.st) / 5;
     }
 
-    public void setDoge() {
-        doge.setText(Integer.toString(doge()));
-    }
-
     public static int doge(){
         return (int) (user.bs + 3);
     }
 
-    public void setDmg() {
-        thrust.setText(Dmg.thrust(user.st));
-        swing.setText(Dmg.swing(user.st));
-    }
-
-    public String getParry(ObservableList<Skill> skills) {
+    public static String getParry(ObservableList<Skill> skills) {
         int resultSkill = user.dx - 4;
         for(Skill skill : skills)
             if(skill.parry && skill.level > resultSkill) resultSkill = skill.level + skill.parryBonus;
         return Integer.toString(3 + resultSkill / 2);
     }
 
-    public void setParry(ObservableList<Skill> skills) {
-        parry.setText(getParry(skills));
-    }
-
-
-    public String getBlock(ObservableList<Skill> skills) {
+    public static String getBlock(ObservableList<Skill> skills) {
         int shield = user.dx - 4;
         int cloak = 0;
         for(Skill skill : skills){
@@ -219,9 +104,5 @@ public class UserParams extends AbstractController {
         if(cloak == 0) cloak = user.dx - 5;
         if(shield >= cloak) return Integer.toString(3 + shield / 2);
         else return Integer.toString(3 + cloak / 2);
-    }
-
-    public void setBlock(ObservableList<Skill> skills) {
-        block.setText(getBlock(skills));
     }
 }
