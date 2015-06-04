df16d5c1f63206113aeede402b3432fb96f2c72a
Игорь Кутявин <igor.kutavin@saltpepper.ru>
2015-06-04 17:46:42 +0300

    remove type column

--- src/ru/gurps/generator/models/Technique.java
+++ src/ru/gurps/generator/models/Technique.java
@@ -1,6 +1,5 @@
 package ru.gurps.generator.models;
 
-import javafx.collections.ObservableList;
 import ru.gurps.generator.Main;
 import ru.gurps.generator.config.Model;
 
@@ -8,7 +7,6 @@ public class Technique extends Model {
     public Integer id;
     public String name;
     public String nameEn;
-    public Integer type;
     public Integer complexity;
     public String defaultUse;
     public String demands;
@@ -26,45 +24,6 @@ public class Technique extends Model {
 
     public String getNameEn() {
         return nameEn;
-    }
-
-    public String getType() {
-        switch(type){
-            case 0: return Main.locale.getString("strength_short");
-            case 1: return Main.locale.getString("dexterity_short");
-            case 2: return Main.locale.getString("intellect_short");
-            case 3: return Main.locale.getString("health_short");
-            case 4: return Main.locale.getString("health_points_short");
-            case 5: return Main.locale.getString("will_short");
-            case 6: return Main.locale.getString("perception_short");
-            case 7: return Main.locale.getString("fatigue_points_short");
-        }
-        return null;
-    }
-
-    public String getTypeFull() {
-        switch(type){
-            case 0: return Main.locale.getString("strength");
-            case 1: return Main.locale.getString("dexterity");
-            case 2: return Main.locale.getString("intellect");
-            case 3: return Main.locale.getString("health");
-            case 4: return Main.locale.getString("health_points");
-            case 5: return Main.locale.getString("will");
-            case 6: return Main.locale.getString("perception");
-            case 7: return Main.locale.getString("fatigue_points");
-        }
-        return null;
-    }
-
-    public void setType(String type) {
-        if(type.equals(Main.locale.getString("strength_short"))) this.type = 0;
-        else if(type.equals(Main.locale.getString("dexterity_short"))) this.type = 1;
-        else if(type.equals(Main.locale.getString("intellect_short"))) this.type = 2;
-        else if(type.equals(Main.locale.getString("health_short"))) this.type = 3;
-        else if(type.equals(Main.locale.getString("health_points_short"))) this.type = 4;
-        else if(type.equals(Main.locale.getString("will_short"))) this.type = 5;
-        else if(type.equals(Main.locale.getString("perception_short"))) this.type = 6;
-        else if(type.equals(Main.locale.getString("fatigue_points_short"))) this.type = 7;
     }
 
     public String getComplexity() {
