26405e12d6d2b40cb6688542d491e96fd8222c10
Игорь Кутявин <igor.kutavin@saltpepper.ru>
2015-06-19 13:06:30 +0300

    add function modifiers()

--- src/ru/gurps/generator/models/Feature.java
+++ src/ru/gurps/generator/models/Feature.java
@@ -1,7 +1,11 @@
 package ru.gurps.generator.models;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import ru.gurps.generator.Main;
 import ru.gurps.generator.config.Model;
+
+import java.util.HashMap;
 
 public class Feature extends Model {
     public Integer id;
@@ -17,6 +21,7 @@ public class Feature extends Model {
     public Boolean cybernetic;
     @Ignore public Boolean add  = false;
     @Ignore public Boolean modifier  = false;
+//    @Ignore public String fullTitle;
 
     public Feature() {
     }
@@ -80,5 +85,20 @@ public class Feature extends Model {
 
     public Boolean getModifier() {
         return modifier;
+    }
+
+    public ObservableList<Modifier> modifiers(int userId){
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("userId", userId);
+        params.put("featureId", id);
+        ObservableList<UserModifier> userModifiers = new UserModifier().where(params);
+        ObservableList<Modifier> modifiers = FXCollections.observableArrayList();
+        for(UserModifier userModifier : userModifiers){
+            Modifier modifier = (Modifier) new Modifier().find(userModifier.modifierId);
+            modifier.level = userModifier.level;
+            modifier.cost = userModifier.cost;
+            modifiers.add(modifier);
+        }
+        return modifiers;
     }
 }
