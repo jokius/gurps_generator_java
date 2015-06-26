1e10806ca0eaa890e62f86f25c60132d088ba36a
Игорь Кутявин <igor.kutavin@saltpepper.ru>
2015-06-26 16:28:50 +0300

    Add updateFormServer button, sendToServer button, add functions

--- src/ru/gurps/generator/controller/LanguagesController.java
+++ src/ru/gurps/generator/controller/LanguagesController.java
@@ -1,5 +1,8 @@
 package ru.gurps.generator.controller;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,9 +11,24 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.HttpHostConnectException;
+import org.apache.http.message.BasicNameValuePair;
 import ru.gurps.generator.Main;
 import ru.gurps.generator.models.Language;
 import ru.gurps.generator.models.UserLanguage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class LanguagesController extends AbstractController {
     public TableView<Language> tableView;
@@ -27,24 +45,13 @@ public class LanguagesController extends AbstractController {
     public TextField costText;
     public Button addButton;
 
-    private ObservableList<Language> languages = FXCollections.observableArrayList();
+    public Button updateFromServer;
+    public Button sedToServer;
+
+    private Language language;
 
     @FXML
     private void initialize() {
-        for(Object object : new Language().all()){
-            Language language = (Language) object;
-            for(Language userLanguage : user.languages()){
-                if(language.id == userLanguage.id){
-                    language.written = userLanguage.written;
-                    language.spoken = userLanguage.spoken;
-                    language.cost = userLanguage.cost;
-                    language.add = true;
-                }
-            }
-
-            languages.add(language);
-        }
-
         ObservableList<String> spokenValues = FXCollections.observableArrayList(
                 Main.locale.getString("not_have"),
                 Main.locale.getString("broken"),
@@ -61,21 +68,21 @@ public class LanguagesController extends AbstractController {
         spokenColumn.setCellFactory(ComboBoxTableCell.forTableColumn(spokenValues));
         spokenColumn.setOnEditCommit(event -> {
             Language language = event.getTableView().getItems().get(event.getTablePosition().getRow());
-            if(!language.getSpoken().equals(event.getNewValue())) language.setSpoken(event.getNewValue());
+            if (!language.getSpoken().equals(event.getNewValue())) language.setSpoken(event.getNewValue());
         });
 
         writtenColumn.setCellValueFactory(new PropertyValueFactory<>("written"));
         writtenColumn.setCellFactory(ComboBoxTableCell.forTableColumn(writtenValues));
         writtenColumn.setOnEditCommit(event -> {
             Language language = event.getTableView().getItems().get(event.getTablePosition().getRow());
-            if(!language.getWritten().equals(event.getNewValue())) language.setWritten(event.getNewValue());
+            if (!language.getWritten().equals(event.getNewValue())) language.setWritten(event.getNewValue());
         });
 
         costColumn.setCellValueFactory(new PropertyValueFactory<>("cost"));
         costColumn.setOnEditCommit(event -> {
-            if(event.getNewValue().equals("0") || !event.getNewValue().matches("\\d+")) return;
+            if (event.getNewValue().equals("0") || !event.getNewValue().matches("\\d+")) return;
             Language language = event.getTableView().getItems().get(event.getTablePosition().getRow());
-            if(language.cost != Integer.parseInt(event.getNewValue()))
+            if (language.cost != Integer.parseInt(event.getNewValue()))
                 language.cost = Integer.parseInt(event.getNewValue());
         });
         costColumn.setCellFactory(TextFieldTableCell.forTableColumn());
@@ -86,12 +93,12 @@ public class LanguagesController extends AbstractController {
         dbColumn.setCellValueFactory(p -> new SimpleBooleanProperty(true));
         dbColumn.setCellFactory(p -> new LanguagesDbButtonCell());
 
-        tableView.setItems(languages);
+        setLanguages();
         tableView.setPlaceholder(new Label(Main.locale.getString("languages_not_found")));
         tableView.setEditable(true);
 
         nameText.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue.equals("")) return;
+            if (newValue.equals("")) return;
             addButton.setDisable(false);
         });
 
@@ -100,19 +107,112 @@ public class LanguagesController extends AbstractController {
         writtenChoiceBox.setItems(writtenValues);
         writtenChoiceBox.getSelectionModel().selectFirst();
         addButton.setOnAction(event -> {
-            Language language = (Language) new Language(nameText.getText()).create();
+            String name = nameText.getText().substring(0, 1).toUpperCase() + nameText.getText().substring(1);
+            language = (Language) new Language(name).create();
             language.spoken = spokenChoiceBox.getSelectionModel().getSelectedIndex();
             language.written = writtenChoiceBox.getSelectionModel().getSelectedIndex();
             language.cost = Integer.parseInt(costText.getText());
             language.add = true;
 
             new UserLanguage(user.id, language.id, language.spoken, language.written, language.cost).create();
-            if(language.cost != 0) setCurrentPoints(Integer.parseInt(user.currentPoints) + language.cost);
-            languages.add(language);
-            tableView.setItems(languages);
+            if (language.cost != 0) setCurrentPoints(Integer.parseInt(user.currentPoints) + language.cost);
+            setLanguages();
             nameText.setText("");
             addButton.setDisable(true);
+            sedToServer.setDisable(false);
         });
+
+        updateFromServer.setOnAction(event -> updateFromServer());
+        sedToServer.setOnAction(event -> sedToServer(language.name));
+    }
+
+    private void updateFromServer() {
+        String repose = getLanguages(1);
+        if (repose.equals("")) return;
+        JsonObject json = new JsonParser().parse(repose).getAsJsonObject();
+        boolean next = newLanguages(json);
+        HashMap<String, Object> pages = pages(json.getAsJsonObject("pagination"));
+
+        if (next) {
+            while (next && (Boolean) pages.get("next")) {
+                json = new JsonParser().parse(getLanguages((int) pages.get("page") + 1)).getAsJsonObject();
+                pages = pages(json.getAsJsonObject("pagination"));
+                next = newLanguages(json);
+            }
+        }
+
+        setLanguages();
+    }
+
+    private String getLanguages(int page) {
+        try {
+            HttpGet getRequest = new HttpGet("/api/languages?page=" + page);
+            HttpResponse httpResponse = httpClient.execute(server, getRequest);
+            HttpEntity entity = httpResponse.getEntity();
+            if (entity == null) return "";
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((entity.getContent())));
+            return br.readLine();
+
+        } catch (HttpHostConnectException e) {
+            return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private void sedToServer(String name) {
+        sedToServer.setDisable(true);
+
+        try {
+            HttpPost httpPost = new HttpPost("/api/languages");
+            List<NameValuePair> params = new ArrayList<>();
+            params.add(new BasicNameValuePair("language", name));
+            params.add(new BasicNameValuePair("password", "secret"));
+            httpPost.setEntity(new UrlEncodedFormEntity(params,"UTF-8"));
+            HttpResponse httpResponse = httpClient.execute(server, httpPost);
+            HttpEntity entity = httpResponse.getEntity();
+
+            if (httpResponse.getStatusLine().getStatusCode() == 204) return;
+            BufferedReader br = new BufferedReader(new InputStreamReader((entity.getContent())));
+            String response = br.readLine();
+            String error = new JsonParser().parse(response).getAsJsonObject().get("error").getAsString();
+            System.out.println(error);
+        } catch (HttpHostConnectException ignore) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setLanguages() {
+        ObservableList<Language> languages = FXCollections.observableArrayList();
+        for (Object object : new Language().all()) {
+            Language language = (Language) object;
+            for (Language userLanguage : user.languages()) {
+                if (language.id == userLanguage.id) {
+                    language.written = userLanguage.written;
+                    language.spoken = userLanguage.spoken;
+                    language.cost = userLanguage.cost;
+                    language.add = true;
+                }
+            }
+
+            languages.add(language);
+        }
+
+        tableView.setItems(languages);
+    }
+
+    private boolean newLanguages(JsonObject json) {
+        if (json.get("languages").getAsJsonArray().size() == 0) return false;
+        for (JsonElement language : json.get("languages").getAsJsonArray()) {
+            String name = language.getAsJsonObject().get("name").getAsString();
+            if (((Language) new Language().find_by("name", name)).id != null) return false;
+            new Language(name).create();
+        }
+
+        return true;
     }
 
     private class LanguagesUserButtonCell extends TableCell<Language, Boolean> {
@@ -140,13 +240,13 @@ public class LanguagesController extends AbstractController {
         @Override
         protected void updateItem(Boolean t, boolean empty) {
             super.updateItem(t, empty);
-            if(getGraphic() != null && t == null) setGraphic(null);
-            if(empty) {
+            if (getGraphic() != null && t == null) setGraphic(null);
+            if (empty) {
                 setGraphic(null);
                 return;
             }
             Language language = (Language) getTableRow().getItem();
-            if(language == null) return;
+            if (language == null) return;
             setGraphic(language.add ? removeButton : addButton);
         }
     }
@@ -157,23 +257,22 @@ public class LanguagesController extends AbstractController {
         LanguagesDbButtonCell() {
             removeButton.setOnAction(t -> {
                 Language language = (Language) getTableRow().getItem();
-                language.delete();
                 new UserLanguage().delete_all(new UserLanguage().where("languageId", language.id));
-                languages.remove(language);
-                tableView.setItems(languages);
+                language.delete();
+                setLanguages();
             });
         }
 
         @Override
         protected void updateItem(Boolean t, boolean empty) {
             super.updateItem(t, empty);
-            if(getGraphic() != null && t == null) setGraphic(null);
-            if(empty) {
+            if (getGraphic() != null && t == null) setGraphic(null);
+            if (empty) {
                 setGraphic(null);
                 return;
             }
             Language language = (Language) getTableRow().getItem();
-            if(language == null) return;
+            if (language == null) return;
             setGraphic(removeButton);
         }
     }
