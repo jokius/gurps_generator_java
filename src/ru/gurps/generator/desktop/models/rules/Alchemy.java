f8fe6e3e195f64e82273c96f547edba2572efc1f
Игорь Кутявин <jok@MacBook-Pro-Igor.local>
2016-01-18 16:08:41 +0300

    Add Alchemy view

--- src/ru/gurps/generator/desktop/models/rules/Alchemy.java
+++ src/ru/gurps/generator/desktop/models/rules/Alchemy.java
@@ -2,6 +2,7 @@ package ru.gurps.generator.desktop.models.rules;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TableRow;
+import ru.gurps.generator.desktop.Main;
 import ru.gurps.generator.desktop.config.Model;
 
 public class Alchemy extends Model {
@@ -36,6 +37,31 @@ public class Alchemy extends Model {
 
     public String getCost() {
         return cost;
+    }
+
+    public String getAlternativeNamesSingle() {
+        if(alternativeNames == null) return "";
+        else return Main.locale.getString("alternative_names")+": " + alternativeNames;
+    }
+
+    public String getDurationSingle(){
+        String dur = duration == null ? Main.locale.getString("no") : duration;
+        return Main.locale.getString("duration") + ": " + dur;
+    }
+
+    public String getFormSingle(){
+        String formS = form == null ? Main.locale.getString("no") : form;
+        return Main.locale.getString("form") + ": " + formS;
+    }
+
+    public String getRecipeSingle(){
+        String rec = recipe == null ? Main.locale.getString("no") : recipe;
+        return Main.locale.getString("recipe") + ": " + rec;
+    }
+
+    public String getCostSingle(){
+        String costS = cost == null ? Main.locale.getString("no") : cost;
+        return Main.locale.getString("cost") + ": " + costS;
     }
 
     public void setAddAndColorRow(Boolean sAdd){
